863dc455ceffcb83e220678d5b06035a2a5afe8c
Anass Nammal <anassnammal@gmail.com>
2024-05-02 10:42:22 +0100

    nginx is complete!

---
+++ srcs/requirements/nginx/tools/init_nginx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+STEPS=(
+    "apt-get update && apt-get install nginx openssl -y"
+    "mkdir -p /etc/nginx/ssl"
+    "openssl req -x509 -nodes -out /etc/nginx/ssl/inception.crt -keyout //etc/nginx/ssl/inception.key -subj \"/C=MA/ST=Khouribga/L=Khouribga/O=42/OU=1337kh/CN=anammal.42.fr/UID=anammal\""
+    "mkdir -p /var/run/nginx"
+    "chmod 0755 /var/www/html"
+    "chown -R www-data:www-data /var/www/html"
+)
+
+for step in "${STEPS[@]}"
+do
+    eval $step
+    if [[ $? -ne 0 ]]; then
+        exit $?
+    fi
+done
