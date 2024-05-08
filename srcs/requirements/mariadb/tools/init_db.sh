967ca6b3d8058ad43de6d87f7bbd2bd34d4e3ed1
Anass Nammal <anassnammal@gmail.com>
2024-05-08 18:57:44 +0100

    UPDATE: MariaDB init to accommodate changes in the project structure, particularly addressing cases where the database is already initialized

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,20 +1,52 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    init_db.sh                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: anammal <anammal@student.1337.ma>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/05/08 17:34:22 by anammal           #+#    #+#              #
+#    Updated: 2024/05/08 18:53:30 by anammal          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 #!/bin/bash
 
-STEPS=(
-    "service mysql start"
-    "mysql -e \"CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;\""
-    "mysql -e \"CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';\""
-    "mysql -e \"GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';\""
-    "mysql -e \"FLUSH PRIVILEGES;\""
-    "mysqladmin -u root -p ${SQL_ROOT_PASSWORD} shutdown"
-)
 
-for step in ${STEPS[@]}
-do
-    eval $step
-    if [[ $? -ne 0 ]]; then
-        exit $?
-    fi
-done
+####### This script is used to initialize and configure the mariadb server #######
 
-rm -rf $0
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# start the mariadb service
+service mariadb start
+
+# arbitrary sleep to wait for the mariadb service to start
+echo "Waiting for the mariadb service to be fully ready..."
+sleep 5
+
+# check if the database is already initialized
+if [ -f /var/lib/mysql/initialized ]; then
+    echo "Database is already initialized"
+    exit 0
+fi
+
+## this part is a set of SQL commands passed to db via "mysql -e" command
+
+# create the database
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+
+# create the user
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+# grant all privileges to the newly created user
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+# set the root password
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+# flush privileges to apply changes
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+
+# shutdown the mariadb service
+mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
